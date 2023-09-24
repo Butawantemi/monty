b5fae0d270abee6a3c9aa313ace80ca1595da202
Butawantemi <japhetbuta@gmail.com>
2023-09-24 23:46:22 +0300

    monty

--- add04.c
+++ add04.c
@@ -1,19 +1,24 @@
 #include "monty.h"
 
+/**
+ * add - adds the top two elements of the stack.
+ * @stack: stack head
+ * @line_number: line number
+ * Return: no return
+*/
 void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *val = NULL;
-	int sum = 0;
-
-	if (!*stack || !(*stack)->next)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		free(*stack);
-		exit(EXIT_FAILURE);
-	}
-	val = (*stack)->next;
-	sum = (*stack)->n;
-	sum += (*stack)->next->n;
-	pop(stack, line_number);
-	val->n = sum;
+stack_t *val = NULL;
+int sum = 0;
+if (!*stack || !(*stack)->next)
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+free(*stack);
+exit(EXIT_FAILURE);
+}
+val = (*stack)->next;
+sum = (*stack)->n;
+sum += (*stack)->next->n;
+pop(stack, line_number);
+val->n = sum;
 }
